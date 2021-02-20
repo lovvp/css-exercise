5b675d37e7a21d2d8cad827f82bdfabc265ed9ee
lovvp <67538804+lovvp@users.noreply.github.com>
2021-02-21 02:27:48 +0900

    JS Home transparent
    home transparent

--- main.js
+++ main.js
@@ -7,6 +7,13 @@ const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
 	if (window.scrollY > navbarHeight) {
 		navbar.classList.add("navbar--dark");
+		// if (window.scrollY < 0.5 * homeHeight) {
+		// 	home.classList.add("home--opacity--50");
+		// 	home.classList.remove("home--opacity--0");
+		// } else {
+		// 	home.classList.add("home--opacity--0");
+		// 	home.classList.remove("home--opacity--50");
+		// }
 	} else {
 		navbar.classList.remove("navbar--dark");
 	}
@@ -33,3 +40,15 @@ const scrollIntoView = (selectors) => {
 	let scrollTo = document.querySelector(selectors);
 	scrollTo.scrollIntoView({ behavior: "smooth" });
 };
+
+const home = document.querySelector("#home");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+	console.log(homeHeight);
+});
+
+document.addEventListener("scroll", () => {
+	const home = document.querySelector(".home__container");
+	console.log(home.getBoundingClientRect().height);
+	home.style.opacity = 1 - window.scrollY / homeHeight;
+});
