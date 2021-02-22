37f588a0c75e1f1be1504cbf67f98600e147b243
lovvp <67538804+lovvp@users.noreply.github.com>
2021-02-22 22:37:42 +0900

    add scroll menu effect
    update scroll menu effect

--- main.js
+++ main.js
@@ -32,6 +32,7 @@ navbarMenu.addEventListener("click", (e) => {
 	}
 	scrollIntoView(link);
 	navbarMenu.classList.remove("open");
+	selectNavItem(target);
 });
 
 navbarToggleBtn.addEventListener("click", () => {
@@ -47,6 +48,7 @@ contact.addEventListener("click", () => {
 const scrollIntoView = (selectors) => {
 	let scrollTo = document.querySelector(selectors);
 	scrollTo.scrollIntoView({ behavior: "smooth" });
+	selectNavItem(navItems[sectionIds.indexOf(selectors)]);
 };
 
 const home = document.querySelector("#home");
@@ -101,6 +103,69 @@ workBtnContainer.addEventListener("click", (e) => {
 });
 
 arrowUp.addEventListener("click", () => {
-	const home = document.querySelector("#home");
-	home.scrollIntoView({ behavior: "smooth" });
+	scrollIntoView("#home");
+});
+
+// 무언가를 만들 때 로직을 작성하는 습관을 갖는 것이 제일중요
+//1. 모든 섹션 요소들과 그에 해당하는 nav__menu__item들을 가지고 온다
+//2. IntersectionObserver를 이용해서 모든섹션들을 관찰한다.
+//3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
+
+const sectionIds = [
+	"#home",
+	"#about",
+	"#skills",
+	"#work",
+	"#testimonials",
+	"#contact",
+];
+
+const sections = sectionIds.map((id) => document.querySelector(id));
+const navItems = sectionIds.map(
+	(id) => document.querySelector(`[data-link="${id}"]`), // 해당 dom의 attribute 를 가져오는 방법
+);
+
+let selectedNavItem = navItems[0];
+let selectedNavIndex = 0;
+function selectNavItem(selected) {
+	selectedNavItem.classList.remove("active");
+	selectedNavItem = selected;
+	selectedNavItem.classList.add("active");
+}
+
+const observerOptions = {
+	root: null,
+	rootMargin: "0px",
+	threshold: 0.3,
+};
+
+const observerCallback = (entries, observer) => {
+	entries.forEach((entry) => {
+		if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+			//빠져 나갈때
+			const index = sectionIds.indexOf(`#${entry.target.id}`);
+			if (entry.boundingClientRect.y < 0) {
+				selectedNavIndex = index + 1;
+			} else {
+				selectedNavIndex = index - 1;
+			}
+		}
+	});
+};
+//!testimonial 이 선택되는 이유는 몇몇 섹션들의 callback함수가 실행되는 것을 볼 수 있다.
+// ->화면을 띄우자 마자 그 화면을 나가기 때문에 callback함수가 실행됨
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach((section) => observer.observe(section));
+//wheel , scroll 의 차이 wheel은 사용자가 직접 스크롤을 해야 발생하는 이벤트
+window.addEventListener("wheel", () => {
+	if (window.scrollY === 0) {
+		selectedNavIndex = 0;
+	} else if (
+		Math.round(window.scrollY + window.innerHeight) >=
+		document.body.clientHeight
+	) {
+		selectedNavIndex = navItems.length - 1;
+	}
+	selectNavItem(navItems[selectedNavIndex]);
 });
